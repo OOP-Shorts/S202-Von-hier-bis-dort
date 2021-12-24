5f1b7ce26c691d212ca0a6d8dcf39b055fc3e15a
Alexander Bazo <alexanderbazo@googlemail.com>
2021-12-24 07:38:23 +0100

    Add solution

--- src/VonHierBisDort.java
+++ src/VonHierBisDort.java
@@ -1,6 +1,13 @@
 public class VonHierBisDort {
 
-    public static void main(String[] args) {
+    private static void printSubset(String[] array, int start, int end) {
+        for(int i = start; i <= end; i++) {
+            System.out.println(array[i]);
+        }
+    }
 
+    public static void main(String[] args) {
+        String[] favourites = {"Gelsenkirchen", "Bochum", "Dortmund", "Witten", "Wattenscheid"};
+        printSubset(favourites, 0, 2);
     }
 }
